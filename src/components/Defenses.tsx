dda19375e36e5c44d158a9c9913458a448d25206
Carlos Aguilar <carlos.figueroa@ikasle.aeg.eus>
2025-10-16 13:56:39 +0200

    [ADD] defenses screen with basic info

--- src/components/Defenses.tsx
+++ src/components/Defenses.tsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
 export default function Defenses( defenseInfo: any ) {
-  return <div>Defenses component</div>
+  return (
+    <div style={{backgroundColor: 'rgba(0,0,0,0.5)', height: 'fit-content', width: '80%', position: 'relative', marginTop: '1vh', textAlign: 'center', display: 'flex', alignItems: 'center', flexDirection: 'column', marginLeft: '10%'}}>
+      <text style={{color: 'yellow', fontSize: '2.5em'}}>DEFENSES</text>
+      <text style={{color: 'lightblue', fontSize: '2em'}}>WALLS</text>
+      <text style={{color: 'white', fontSize: '1.5em'}}>MATERIAL: {defenseInfo.walls.material}</text>
+      <text style={{color: 'white', fontSize: '1.5em'}}>HEIGHT: {defenseInfo.walls.height}m</text>
+      <text style={{color: 'lightblue', fontSize: '2em'}}>ENCHANTMENTS</text>
+      {defenseInfo.walls.enchantments.map((elem: any) => {
+        return <text style={{color: 'white', fontSize: '1.5em'}}>{elem.name} LEVEL: {elem.level}</text>
+      })}
+      <text style={{color: 'lightblue', fontSize: '2em'}}>TOWERS</text>
+      {defenseInfo.towers.map((elem: any) => {
+        return (<text style={{color: 'white', fontSize: '1.5em'}}>{elem.name} HEIGHT: {elem.height}</text>);
+      })}
+    </div>
+  );
 }
